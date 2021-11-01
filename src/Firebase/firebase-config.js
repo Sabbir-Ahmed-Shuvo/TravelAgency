58afd93252d306bfa959fd8a1ea32f4d6c17ba39
sabbirNew <sabbir.ahmed5936@gmail.com>
2021-11-01 15:09:46 +0600

    added all file

--- src/Firebase/firebase-config.js
+++ src/Firebase/firebase-config.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBysm-bKIs_EXeXFxGi-dlmqOOI9CbL2Zk",
-    authDomain: "medical-and-healthcare.firebaseapp.com",
-    projectId: "medical-and-healthcare",
-    storageBucket: "medical-and-healthcare.appspot.com",
-    messagingSenderId: "210523024338",
-    appId: "1:210523024338:web:7e91414bc96c1e017eb60b"
-  };
-  
-  export default firebaseConfig;
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+
+export default firebaseConfig;
